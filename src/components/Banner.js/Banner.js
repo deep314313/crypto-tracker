6c7263a82e60d1b5daf7f885d1d95a445cce5cc0
deep314313 <123625000+deep314313@users.noreply.github.com>
2024-06-25 14:25:55 +0530

    Update Banner.js

--- src/components/Banner.js/Banner.js
+++ src/components/Banner.js/Banner.js
@@ -4,10 +4,10 @@ import Carousel from './Carousel'; // Assuming Carousel component is defined els
 
 const Banner = () => {
   return (
-    <div className="banner" style={{ backgroundImage: "url(./image.png)" }}>
+    <div className="banner" style={{ backgroundImage: "url(./stock.jpeg)" }}>
       <Container className="banner-content d-flex flex-column justify-content-between align-items-center">
         <div className="tagline text-center">
-          <h2 className="fw-bold mb-3 text-gold">Crypto Hunter</h2>
+          <h2 className="fw-bold mb-3 text-gold" style={{ color: '#9ca1a6'}}>Crypto Tracker</h2>
           <p className="text-darkgrey text-capitalize">Get all the Info regarding your favorite Crypto Currency</p>
         </div>
         <div className="carousel d-flex align-items-center">
